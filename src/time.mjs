f29f2a7538037fc90553b3035e31c98c4fc6d04b
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2024-10-09 15:28:52 +0200

    remove one bootstrapNode

--- src/time.mjs
+++ src/time.mjs
@@ -19,7 +19,6 @@ class TimeSynchronizer {
         this.lastSyncedTime = null;
         this.offset = 0; // Time offset between system time and NTP time
         this.#startSyncLoop();
-        this.isRunning = false; //? DEPRECATED ?
     }
 
     getCurrentNtpServer() {
@@ -30,21 +29,6 @@ class TimeSynchronizer {
         this.currentServerIndex = (this.currentServerIndex + 1) % this.ntpServers.length;
     }
 
-    async syncTimeWithRetry_OLD(attempts = this.retryAttempts) { //? DEPRECATED ?
-        console.log(`Attempting NTP sync with ${this.getCurrentNtpServer()}. Attempts left: ${attempts}`);
-        try {
-            await this.syncTimeWithNTP();
-        } catch (err) {
-            if (attempts > 1) {
-                console.warn(`Retrying NTP sync. Rotating to next server.`);
-                this.rotateNtpServer();
-                await this.delay(this.retryDelay);
-                return this.syncTimeWithRetry(attempts - 1);
-            } else {
-                console.error(`Failed to sync with NTP after ${this.retryAttempts} attempts`);
-            }
-        }
-    }
     async syncTimeWithRetry(attempts = this.retryAttempts, delay) {
         console.log(`Attempting NTP sync with ${this.getCurrentNtpServer()}. Attempts left: ${attempts}`);
 
@@ -67,7 +51,6 @@ class TimeSynchronizer {
             await this.syncTimeWithRetry(); // Re-sync every syncInterval with retry
         }
     }
-
     async syncTimeWithNTP() {
         console.log(`Syncing time with NTP server: ${this.getCurrentNtpServer()}`);
         return new Promise((resolve, reject) => {
@@ -84,57 +67,10 @@ class TimeSynchronizer {
             });
         });
     }
-    
-    delay(ms) { //? DEPRECATED ?
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
 
     // Get the synchronized current time
     getCurrentTime() {
         return Date.now() + this.offset;
-    }
-
-    // Schedule the next epoch based on the synchronized time
-    scheduleNextEpoch(callback) {
-        const currentTime = this.getCurrentTime();
-        const timeUntilNextEpoch = this.epochInterval - (currentTime % this.epochInterval);
-        setTimeout(async () => {
-            callback();
-            this.scheduleNextEpoch(callback); // Schedule the next epoch after the current one
-        }, timeUntilNextEpoch);
-    }
-
-    // Schedule the next round based on the synchronized time
-    scheduleNextRound(callback) {
-        const currentTime = this.getCurrentTime();
-        const timeUntilNextRound = this.roundInterval - (currentTime % this.roundInterval);
-        setTimeout(async () => {
-            callback();
-            this.scheduleNextRound(callback); // Schedule the next round after the current one
-        }, timeUntilNextRound);
-    }
-
-    // Start the periodic synchronization with the NTP server with retries
-    /*startSyncLoop() { // DEPRECATED
-        this.syncTimeWithRetry(); // Initial sync with retry
-        setInterval(() => {
-            this.syncTimeWithRetry(); // Re-sync every syncInterval with retry
-        }, this.syncInterval);
-    }*/
-
-    // Start the synchronization loop
-    async startSyncLoop() { //? DEPRECATED ?
-        if (this.isRunning) {
-            console.warn('TimeSynchronizer is already running.');
-            return;
-        }
-        this.isRunning = true;
-        console.log('Starting TimeSynchronizer...');
-
-        while (this.isRunning) {
-            await this.syncTimeWithRetry();
-            await this.delay(this.syncInterval);
-        }
     }
 }
 
