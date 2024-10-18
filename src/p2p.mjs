b090efa5a132a91799d63e6e0355035abf25205c
sinedots <>
2024-10-18 10:56:29 +0200

    p2p bootstraping

--- src/p2p.mjs
+++ src/p2p.mjs
@@ -161,7 +161,7 @@ class P2PNetwork extends EventEmitter {
 
                 const ma = multiaddr(addr);
                 await this.p2pNode.dial(ma, { signal: AbortSignal.timeout(this.options.dialTimeout) });
-                await this.p2pNode.components.connectionManager.openConnection(ma);
+                //await this.p2pNode.components.connectionManager.openConnection(ma);
                 this.logger.info({ component: 'P2PNetwork', bootstrapNode: addr }, 'Connected to bootstrap node');
             } catch (err) {
                 this.logger.error({ component: 'P2PNetwork', bootstrapNode: addr, error: err.message }, 'Failed to connect to bootstrap node');
@@ -175,7 +175,6 @@ class P2PNetwork extends EventEmitter {
         this.p2pNode.addEventListener('peer:disconnect', this.#handlePeerDisconnect);
         this.p2pNode.addEventListener('peer:discovery', (event) => {
             const peerId = event.detail.id + " " + event.detail.multiaddrs.toString();
-            this.p2pNode.components.connectionManager.openConnection(event.detail.multiaddrs[0]);
             this.logger.info({ peerId }, 'Peer discovered');
         });
         this.p2pNode.services.pubsub.addEventListener('message', this.#handlePubsubMessage);
