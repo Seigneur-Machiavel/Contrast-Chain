e3fbed5fd246c13e4a71ca6a1e922060c18774cc
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2024-10-25 14:11:32 +0200

    fix miner timing

--- src/miner.mjs
+++ src/miner.mjs
@@ -220,7 +220,7 @@ export class Miner {
         const workersStatus = [];
         let lastHashTime = this.timeSynchronizer.getCurrentTime();
         while (!this.terminated) {
-            const delayBetweenMining = this.roles.includes('validator') ? 1 : 1;
+            const delayBetweenMining = this.roles.includes('validator') ? 20 : 10;
             await new Promise((resolve) => setTimeout(resolve, delayBetweenMining));
 
             const preshotedPowReadyToSend = this.preshotedPowBlock ? this.preshotedPowBlock.timestamp <= this.timeSynchronizer.getCurrentTime() : false;
