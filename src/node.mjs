183a713d8748a770782539a81089bda473a374a0
Seigneur-Machiavel <guibi50ae@hotmail.fr>
2024-11-04 17:21:46 +0100

    another softfork test protocol

--- src/node.mjs
+++ src/node.mjs
@@ -561,7 +561,7 @@ ${hashConfInfo.message}`);
                     });
                     break;
                 case 'new_block_candidate':
-                    if (data.index > 10 && data.index < 15) { return; }
+                    if (this.blockCandidate < 11 && data.index === 10) { return; }
                     if (!this.roles.includes('miner')) { break; }
                     if (!this.roles.includes('validator')) { break; }
                     if (this.miner.highestBlockIndex > data.index) { // avoid processing old blocks
@@ -588,7 +588,7 @@ ${hashConfInfo.message}`);
                     this.miner.updateBestCandidate(data);
                     break;
                 case 'new_block_finalized':
-                    if (data.index > 10 && data.index < 15) { return; }
+                    if (this.blockCandidate < 11 && data.index === 10) { return; }
                     if (this.syncHandler.isSyncing || this.opStack.syncRequested) { return; }
                     //TODO: remove this test code
                     /*if (data.index % 2 === 0 && !this.testRejectedIndexes.includes(data.index)) {
